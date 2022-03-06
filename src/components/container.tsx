adabe26bab041877dab28411c734fe95beff451f
liuhao <czliuhao@qq.com>
2022-03-07 00:18:34 +0800

    gatsby学习

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import * as ContainerStyles from './styles/container.module.scss';
-
-console.log(ContainerStyles)
+import { useStaticQuery, graphql } from 'gatsby';
 
 const Container: React.FC = ({ children }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
+
     return (
         <>
             <div className={ContainerStyles.container}>
+                <h1>
+                    {data.site.siteMetadata.title}
+                </h1>
                 {children}
             </div>
         </>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Container from '../components/container';
+import { graphql } from 'gatsby'
 
-const About: React.FC = () => {
+const About: React.FC = ({ data }) => {
     return (
         <Container>
             <div style={{ 'color': 'red' }}>
+                <h1>About {data.site.siteMetadata.title}</h1>
                 <h1>I am about component.</h1>
             </div>
         </Container>
@@ -12,3 +14,13 @@ const About: React.FC = () => {
 }
 
 export default About;
+
+export const query = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
