d82c5d305a9831d31cb05677ebfd0eecdaf908f8
czliuhao <czliuhao@qq.com>
2022-03-14 00:18:48 +0800

    add gatsby css and images and media

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from './../components/layout';
 import { Link } from 'gatsby';
+import girlImg from './../images/girl.jpeg';
 
 const App: React.FC = () => {
     return (
@@ -8,6 +9,7 @@ const App: React.FC = () => {
             <Layout>
                 <h1> GATSBY_DISK_API: {process.env.GATSBY_DISK_API}</h1>
                 <h1>SECRET:{process.env.SECRET}</h1>
+                <img src={girlImg}></img>
                 <Link to='/my-files/'>My Files</Link>
             </Layout>
         </>
--- src/pages/my-files.tsx
+++ src/pages/my-files.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import Layout from './../components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
+
 
 const MyFiles: React.FC = () => {
+
+    const data = useStaticQuery(graphql`
+    {
+        allFile(filter: { extension: {eq: "jpeg"}}){
+            edges {
+                node {
+                    publicURL
+                    name
+                }
+            }
+        }
+    }
+    `)
+
     return (
         <Layout>
             <h1>Hello World</h1>
+            <ul>
+                {data.allFile.edges.map((item, inx) => {
+                    return (<li key={inx}>
+                        <a href={item.node.publicURL} target="_blank">
+                            {item.node.name}
+                        </a>
+                    </li>)
+                })
+                }
+            </ul>
         </Layout>
     )
 };
